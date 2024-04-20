0a9542c53a7dd93ec55850863215e803e495fdb1
andrezipretin <andrcosta72@gmail.com>
2024-04-20 20:37:36 -0300

    usando vector, organizando

--- xadrez/tabuleiro.cpp
+++ xadrez/tabuleiro.cpp
@@ -39,27 +39,47 @@ class Personagem{
 			Maltura=altura;
 		}
 };
+void tabuleiro(int aux,int aux1,std::vector <Personagem> meuspersonagens){
+	int k,i=0,j;
+	for(j=0;j<aux;j++){
+		for(k=0;k<aux1;k++){
+			if(meuspersonagens[i].getY()==j && meuspersonagens[i].getX()==k){
+				std::cout<<"["<<meuspersonagens[i].getDesenho()<<"]";
+				i++;
+				}
+			else{
+				std::cout<<"[.]";
+			}
+			}	
+			std::cout<<std::endl;
+		}
+}
 int main(){
 	int i,j,k;
+	Personagem aux;
 	std::vector <Personagem> meuspersonagens;
+	Personagem andre(1,7,"4");
+	Personagem joao(2,8,"3");
+	Personagem joao1(3,6,"2");
+	Personagem joao2(4,5,"1");
 
-	Personagem andre(3,3,"X");
-	andre.valoresTabuleiro(3,3);
-	Personagem joao(2,1,"O");
 	meuspersonagens.push_back(andre);
 	meuspersonagens.push_back(joao);
+	meuspersonagens.push_back(joao1);
+	meuspersonagens.push_back(joao2);
 
-	for(i=0;i<meuspersonagens.size();i++){
-		for(j=0;j<8;j++){
-			for(k=0;k<8;k++){
-				if(meuspersonagens[i].getY()-1==j && meuspersonagens[i].getY()-1==k){
-					std::cout<<"["<<meuspersonagens[i].getDesenho()<<"]";
-				}
-				else{
-					std::cout<<"[.]";
-				}
-			  }
-			std::cout<<std::endl;
-			}	
+	for(j=0;j<meuspersonagens.size();j++){
+		for(i=0;i<meuspersonagens.size()-1;i++){
+			if(meuspersonagens[i].getY()>meuspersonagens[i+1].getY()){
+			 aux=meuspersonagens[i];	
+		 	 meuspersonagens[i]=meuspersonagens[i+1];
+		 	 meuspersonagens[i+1]=aux;
+			}
 		}
+	}
+	
+	for(j=0;j<meuspersonagens.size();j++){
+	std::cout<<"["<<meuspersonagens[j].getDesenho()<<"]"<<std::endl;
+	}
+	tabuleiro(8,8,meuspersonagens);
 }
