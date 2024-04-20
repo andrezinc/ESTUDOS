04b34ae4c8fd46daf3de29d2ee6ed07ba333f79a
Matsumotuo <joaovitormatsumoto@hotmail.com>
2024-04-20 16:48:52 -0300

    VEJA AÌ

---
+++ numeros_romanos/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+   
+    public static void main(String[] args) {
+        
+        Scanner leitor = new Scanner(System.in);
+        
+        int[] vNum = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        
+        String[] vRomano = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        
+    }
+}
