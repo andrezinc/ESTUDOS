4d344a8d2d8bfc78744db992219e32fa5b8ff925
andrezipretin <andrcosta72@gmail.com>
2024-04-20 17:49:43 -0300

    solução pelo matsmulove

--- numeros_romanos/romanos.cpp
+++ numeros_romanos/romanos.cpp
@@ -2,13 +2,12 @@
 #include <stdlib.h>
 #include <iostream>
 #include <string.h>
-
-
 int main(){
 	int numero,transmacao;
 	
-	std::string romanos[20] = {"I","II","III","IV","V","VI","VII","VIII","IX","X"};
-	
-	for(int i=0;i<10;i++)
+
+	std::string romanos[20] = {"M","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+	int numeros[20]=          {1000,500,400,100,90,  50,  40 , 10 , 9 , 5  , 4  , 1};
+		
 		std::cout<<romanos[i]<<std::endl;
 }
